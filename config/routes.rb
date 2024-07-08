380ad2d64289380b42774fe90008ecd054c7abd3
EC2 Default User <ec2-user@ip-172-31-14-116.ap-northeast-1.compute.internal>
2024-07-08 04:52:29 +0000

    [Finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+  #get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/top' => 'homes#top'
+  #post 'lists' => 'lists#create'
+  #get 'lists' => 'lists#index'
+  # get 'lists/show' ##自動でshowアクションのルーティングが作成されているので、削除（第4章）
+  #get 'lists/:id' => 'lists#show', as: 'list' ##.../lists/1 や .../lists/3と言ったものに該当
+  #get 'lists/edit'
+  #get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  #patch 'lists/:id' => 'lists#update', as: 'update_list'
+  #delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  
+  resources :lists
 end
